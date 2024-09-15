6342d999438b339f67ee003fce27a63f29a32fff
tsaniyakhamal123 <tsaniyakhamal@gmail.com>
2024-09-16 00:09:00 +0700

    update server js, user and film schema

--- server.js
+++ server.js
@@ -32,12 +32,68 @@ mongoose.connect(process.env.mongoDBURI)
     console.log(err);
   });
 
+// IMPORT MODEL
+const Film = require("./api/models/Film");
+const User = require("./api/models/User");
+
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Kelompok 13 Backend Service");
 });
 app.use("/user", require("./api/routes/UserRoutes"));
-app.use("/film", require("./api/routes/FilmRoutes"));
+
+
+// Get all films
+app.get('/film', async (req, res) => {  
+  try {
+    const films = await Film.find({});  
+    res.status(200).json(films);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//get films by tittle
+app.get('/film/:title', async (req, res) => {
+  try {
+    const { title } = req.params;  
+    const film = await Film.findOne({ title });  
+
+    if (!film) {
+      return res.status(404).json({ message: `Film with title ${title} not found` });
+    }
+
+    res.status(200).json(film);  
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//get all user
+app.get('/user', async (req, res) => {  
+  try {
+    const users = await User.find({});  
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//get user by name
+app.get('/user/:name', async (req, res) => {  
+  try {
+    const { name } = req.params;  
+    const user = await User.findOne({ name: { $regex: new RegExp(name, 'i') } });  //suapaya besar kecil huruf ga berpengaruh
+    if (!user) {
+      return res.status(404).json({ message: `User with name ${name} not found` });
+    }
+
+    res.status(200).json(user);  
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 // APP START
 app.listen(3000, () => {
