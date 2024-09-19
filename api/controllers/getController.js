edef02b538b5a02cd8f0d2c5154f489b7128374e
johanpramudito <ajohanpramudito@gmail.com>
2024-09-19 08:15:54 +0700

    feat: getFilm by Genre

---
+++ api/controllers/getController.js
@@ -0,0 +1,71 @@
+const Film = require('../models/Film');
+const User = require('../models/User');
+
+// Get all films
+exports.getAllFilms = async (req, res) => {
+  try {
+    const films = await Film.find({});
+    res.status(200).json(films);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get film by title
+exports.getFilmByTitle = async (req, res) => {
+  try {
+    const { title } = req.params;
+    const film = await Film.findOne({ title });
+
+    if (!film) {
+      return res.status(404).json({ message: `Film with title ${title} not found` });
+    }
+
+    res.status(200).json(film);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get films by genre
+exports.getFilmsByGenre = async (req, res) => {
+  try {
+    const { genre } = req.params;
+    const films = await Film.find({ genre: { $regex: new RegExp(genre, 'i') } }); // pencarian genre tidak case-sensitive
+
+    if (films.length === 0) {
+      return res.status(404).json({ message: `No films found in genre ${genre}` });
+    }
+
+    res.status(200).json(films);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get user by name
+exports.getUserByName = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const user = await User.findOne({ name: { $regex: new RegExp(name, 'i') } }); // biar kapital engganya ga berpengaruh
+
+    if (!user) {
+      return res.status(404).json({ message: `User with name ${name} not found` });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- api/routes/FilmRoutes.js
+++ api/routes/FilmRoutes.js
@@ -2,10 +2,14 @@ const FilmRouter = require("express").Router();
 
 const { createFilm } = require("../controllers/postController");
 const { deleteFilm } = require("../controllers/deleteController");
+const { updateFilm } = require("../controllers/putController");
+const { getAllFilms, getFilmByTitle, getFilmsByGenre } = require("../controllers/getController");
 
 FilmRouter.post("/createFilm", createFilm);
 // UserRouter.get("/read", readUser);
 // UserRouter.put("/update/:id", updateUser);
 FilmRouter.delete("/deleteFilm/:id", deleteFilm);
-
+FilmRouter.get("/getFilm", getAllFilms); // Untuk GET /film
+FilmRouter.get("/getFilm/:title", getFilmByTitle); // Untuk GET /film/:title
+FilmRouter.get("/genre/:genre", getFilmsByGenre)
 module.exports = FilmRouter;
